e19d4dae4ed7483dfe4aa589bfa681fd266f754f
Cássio Paixão <cassiopaixaoti@gmail.com>
2018-10-07 14:41:46 -0300

    add worker pub

--- device-fake-service/src/middleware/config/Application.ts
+++ device-fake-service/src/middleware/config/Application.ts
@@ -1,14 +1,13 @@
-import {logger} from '../common/Logging'
-import {ExpressConfig} from './Express';
-import * as config from 'config';
-import * as spdy from 'spdy';
-import * as path from 'path';
-import * as fs from 'fs';
-import {KnotSocket} from './SocketIO'
-import {EurekaService} from "./Eureka";
+import { logger } from "../common/Logging";
+import { ExpressConfig } from "./Express";
+import * as config from "config";
+import * as spdy from "spdy";
+import * as path from "path";
+import * as fs from "fs";
+import { KnotSocket } from "./SocketIO";
+// import {EurekaService} from "./Eureka";
 
 export class Application {
-
   server: any;
   express: ExpressConfig;
 
@@ -17,17 +16,17 @@ export class Application {
   constructor() {
     this.express = new ExpressConfig();
 
-    const certsPath = path.resolve('certs');
+    const certsPath = path.resolve("certs");
     const options = {
       key: fs.readFileSync(certsPath + "/server.key"),
       cert: fs.readFileSync(certsPath + "/server.crt")
     };
 
-    const port = config.get('express.port');
+    const port = config.get("express.port");
 
-    this.server = spdy.createServer(options, this.express.app)
+    this.server = spdy
+      .createServer(options, this.express.app)
       .listen(port, (error: any) => {
-
         if (error) {
           logger.error("failed to start server with ssl", error);
           return process.exit(1);
@@ -37,20 +36,19 @@ export class Application {
       Server Started! Express: http://localhost:${port}
       Starting KNoT cloud client...
       ------------------------------------------------------
-      `)
+      `);
       });
 
     new KnotSocket();
-    EurekaService.builder().start();
-    process.on('SIGINT', () => {
-      logger.info(`
-      -------------------------------------------------- 
-      Stopping KNoT cloud client...
-      ------------------------------------------------------
-      `);
-      EurekaService.builder().stop();
-      this.server.close()
-    });
+    // EurekaService.builder().start();
+    // process.on('SIGINT', () => {
+    //   logger.info(`
+    //   --------------------------------------------------
+    //   Stopping KNoT cloud client...
+    //   ------------------------------------------------------
+    //   `);
+    //   EurekaService.builder().stop();
+    //   this.server.close()
+    // });
   }
-
 }
--- waterflow-service/src/middleware/config/Socket.ts
+++ waterflow-service/src/middleware/config/Socket.ts
@@ -1,52 +1,46 @@
-import * as io from 'socket.io';
-import { logger } from '../../middleware/common/Logging';
-import { WaterFlowUtils } from '../../middleware/common/Utils';
-import { WaterFlowDataAgent } from '../../data-layer/data-agents/WaterFlowDataAgent';
-
+import * as io from "socket.io";
+import { logger } from "../../middleware/common/Logging";
+import { WaterFlowUtils } from "../../middleware/common/Utils";
+import { WaterFlowDataAgent } from "../../data-layer/data-agents/WaterFlowDataAgent";
 
 export class KnotSocket {
-
   waterFlowUtils: WaterFlowUtils;
   waterFlowDataAgent: WaterFlowDataAgent;
-  
 
   constructor() {
-
     this.waterFlowUtils = new WaterFlowUtils();
     this.waterFlowDataAgent = new WaterFlowDataAgent();
 
     const url = `http://knot-test.cesar.org.br:3000`;
-    const socket = require('socket.io-client')(url);
+    const socket = require("socket.io-client")(url);
 
-    socket.on('connect', () => {
-    logger.info('Smart Hydrometer connected');
-      socket.emit('identity', {
-        uuid: 'fdf6cbee-1a70-4099-8285-1e300a8a0000',
-        token: '1c698e0cb986d279855ff7653428fe83c8b39cac'
+    socket.on("connect", () => {
+      logger.info("Smart Hydrometer connected");
+      socket.emit("identity", {
+        uuid: "fdf6cbee-1a70-4099-8285-1e300a8a0000",
+        token: "1c698e0cb986d279855ff7653428fe83c8b39cac"
       });
-
     });
 
-    socket.on('ready', () => {
-      logger.info('Smart Hydrometer has started');
+    socket.on("ready", () => {
+      logger.info("Smart Hydrometer has started");
       setInterval(() => {
-
         const flowRate = this.waterFlowUtils.randomInRange(66.12345, 125.99999);
-        socket.emit('message', {
-          devices: 'fdf6cbee-1a70-4099-8285-1e300a8a0000',
+        socket.emit("message", {
+          devices: "fdf6cbee-1a70-4099-8285-1e300a8a0000",
           message: { flowRate }
         });
 
-        console.log('sent flow rate:', flowRate);
+        console.log("sent flow rate:", flowRate);
 
-        socket.on('message', data => {
+        socket.on("message", data => {
           // this.waterFlowDataAgent.createNewWaterFlow(data);
         });
       }, 1000);
     });
 
-    socket.on('disconnect', () => {
-      console.log('Smart Hydrometer Client has disconnected...');
+    socket.on("disconnect", () => {
+      console.log("Smart Hydrometer Client has disconnected...");
     });
   }
 }
@@ -93,4 +87,3 @@ export class KnotSocket {
 
 //   return io;
 // };
-
