3827c6a5680be6c32ca3f2ffe977f5721bfe6ceb
Cássio Paixão <cassiopaixaoti@gmail.com>
2018-10-25 01:06:38 -0300

    simulate device

--- device-fake-service/src/data-layer/data-agents/WaterFlowStreamAgent.ts
+++ device-fake-service/src/data-layer/data-agents/WaterFlowStreamAgent.ts
@@ -1,4 +1,5 @@
 import WaterflowPub from "../../middleware/common/WaterflowPub";
+const request = require("request");
 
 export class WaterFlowDataAgent {
   private waterflowPub: WaterflowPub;
@@ -9,11 +10,34 @@ export class WaterFlowDataAgent {
   }
 
   async createNewWaterFlow(data: any) {
-    this.waterFlow = {
-      uuid: data.uuid,
-      flowRate: data.message.flowRate,
-      anamolia: data.anamolia
+    // this.waterFlow = {
+    //   uuid: data.uuid,
+    //   flowRate: data.message.flowRate,
+    //   anamolia: data.anamolia
+    // };
+
+    const dateTime = new Date().getTime();
+    const timestamp = Math.floor(dateTime / 1000);
+
+    const dataT = {
+      userNumber:
+        "dc099880ee7d2b16e956b33cbd8b71d7cf5fdb85a3d14aea434572936a0afbd7",
+      address: "04f4dd31-efde-4c71-9e40-06fc5a38083b01" + timestamp,
+      cost: data.cost,
+      anomalia: data.anamolia
     };
-    this.waterflowPub.publish(this.waterFlow);
+    // this.waterflowPub.publish(this.waterFlow);
+    request.post(
+      {
+        headers: { "content-type": "application/json" },
+        url: "http://localhost:9443/api/register/",
+        form: dataT
+      },
+      function(error, response, body) {
+        console.log("ok");
+        // console.log("response", response);
+        // console.log("error", error);
+      }
+    );
   }
 }
