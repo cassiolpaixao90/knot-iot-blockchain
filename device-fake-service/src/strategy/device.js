5d8aa77e81b5c4e61c87eae0a662f0455b84ca43
Cássio Paixão <cassiopaixaoti@gmail.com>
2018-11-05 18:56:44 -0300

    change method publish and subscribe

--- device-fake-service/src/strategy/device.js
+++ device-fake-service/src/strategy/device.js
@@ -4,15 +4,14 @@ const deviceWorker = require('../workers/worker');
 class DeviceStrategy {
   static water() {
     setInterval(() => {
-      // eslint-disable-next-line no-new-wrappers
       const flowRate = new String(deviceUtil.randomInRange(66.12345, 125.99999));
       const data = {
         uuid: 'fdf6cbee-1a70-4099-8285-1e300a8a0000',
         cost: flowRate.valueOf(),
         anamolia: '0'
       };
-      deviceWorker.publish('jobs', new Buffer(JSON.stringify(data)), 10000);
-    }, 10000);
+      deviceWorker.publish('jobs', new Buffer(JSON.stringify(data)), 1000);
+    }, 1000);
   }
 
   static anomalia() {
@@ -22,15 +21,15 @@ class DeviceStrategy {
         cost: '',
         anamolia: '1'
       };
-      console.log(data);
-    }, 20000);
+      deviceWorker.publish('jobs', new Buffer(JSON.stringify(data)), 2000);
+    }, 2000);
   }
 
   static noValue() {
     setInterval(() => {
       const data = {};
-      console.log('nodata', data);
-    }, 30000);
+      deviceWorker.publish('jobs', new Buffer(JSON.stringify(data)), 3000);
+    }, 3000);
   }
 }
 
